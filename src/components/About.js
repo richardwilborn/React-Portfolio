677cc5c79da4934d793d817f65e3698f1f3d9db8
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-11-02 13:19:14 -0500

    removed some images and updated logo

--- src/components/About.js
+++ src/components/About.js
@@ -32,7 +32,7 @@ export default function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./coding.svg"
+            src="./Richard Wilborn white.webp"
           />
         </div>
       </div>
